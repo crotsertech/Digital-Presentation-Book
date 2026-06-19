dd531e41b22d42e29e61f2709379bae94ddcc13c
N. T. Crotser <ntc@crotser.dev>
2026-06-19 15:05:28 -0500

    Initial Commit

---
+++ Digital Presentation Book/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Digital Presentation Book
+//
+//  Created by N. T. Crotser on 2026/6/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Digital Presentation Book/Digital_Presentation_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_Presentation_BookApp.swift
+//  Digital Presentation Book
+//
+//  Created by N. T. Crotser on 2026/6/19.
+//
+
+import SwiftUI
+
+@main
+struct Digital_Presentation_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
